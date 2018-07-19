582f6ea4eecdf3717975e89214b6a171020986d8
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2018-07-19 09:41:54 +0300

    FPS counter added

---
+++ Assets/Scripts/UI/FPS.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPS : MonoBehaviour
+{
+    [SerializeField]
+    private Text FPSText;
+
+    private float Timer = 0f;
+    private int Counter = 0;
+
+    private void Update()
+    {
+        Timer += Time.deltaTime;
+        Counter++;
+        if (Timer > 1)
+        {
+            FPSText.text = Counter.ToString();
+            Timer = Timer % 1;
+            Counter = 0;
+        }
+    }
+}
